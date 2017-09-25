7333c72ca2a0553d852c196d47ab0c2555033346
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-25 16:10:27 -0500

    class App

--- lab-beeksma/src/main.js
+++ lab-beeksma/src/main.js
@@ -3,7 +3,21 @@ const css = require('./style/main.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'I am the state'
+    };
+  }
+  render(){
+    return(
+      <h1>{this.state.content}</h1>
+    );
+  }
+}
+
 ReactDOM.render(
-  <p>Hello, world!</p>,
+  <App />,
   document.getElementById('cowStuff')
 );
